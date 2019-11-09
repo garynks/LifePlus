947ec701400041e507ab1e5aad27974ef7e82a98
rotenmiar <56034486+rotenmiar@users.noreply.github.com>
2019-11-09 14:00:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -21,15 +21,10 @@ class Window(Frame):
     def init_window(self):
 
         # changing the title of our master widget
-        self.master.title("GUI")
+        self.master.title("LifePlus")
 
         # allowing the widget to take the full space of the root window
         self.pack(fill=BOTH, expand=1)
-
-        # creating a button instance
-        # quitButton = Button(self, text="Quit")
-
-        # placing the button on my window
-        # quitButton.place(x=0, y=0)
+        
 
 main()
