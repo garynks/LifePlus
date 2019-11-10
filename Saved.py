3dc064b6976eeea050fc8aa441525f4e8f4ef750
rotenmiar <56034486+rotenmiar@users.noreply.github.com>
2019-11-09 19:35:23 -0700

    added goal saving and loading

--- Saved.py
+++ Saved.py
@@ -13,7 +13,7 @@ class Saved:
 
     def makefile(self):
         f = open("saved.txt","w+")
-        f.write("Name:\n\nGoals:\n\nHabits:\n\n")
+        f.write("Name:\n\nGoals:\nHabits:\n\n")
 
     def savename(self,text):
         f = open("saved.txt","r")
@@ -32,3 +32,38 @@ class Saved:
             if 'Name:' in data[i]:
                 name = data[i+1][0:len(data[i+1])-1]
                 return name
+
+    def savegoal(self,goal):
+        f = open("saved.txt", "r")
+        data = f.readlines()
+        goal_line = 0  # save the line number of where the goal starts to get listed
+        goalcount = 1  # this is used as number to be written in front of the goal
+
+        for i in range(len(data)):
+            if 'Goals:' in data[i]:
+                goal_line = i + 1
+
+        while data[goal_line] != 'Habits:\n':
+            goalcount += 1
+            goal_line += 1
+
+        data.insert(goal_line,(str(goalcount) + '. ' + goal + '\n'))  # example output : 1. insert goal here (newline)
+
+        f = open("saved.txt", "w")
+        f.writelines(data)
+
+    def loadgoal(self):
+        f = open("saved.txt","r")
+        data = f.readlines()
+        goal_line = 0  # save the line number of where the goal starts to get listed
+        goals = []
+
+        for i in range(len(data)):
+            if 'Goals:' in data[i]:
+                goal_line = i+1
+
+        while data[goal_line] != 'Habits:\n':
+            goals.append(data[goal_line])
+            goal_line += 1
+
+        return goals
