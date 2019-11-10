98f72c98efc009c61c302bce0397cc0679faacb4
Della <humanita.1999@gmail.com>
2019-11-09 18:50:08 -0700

    new format of main by Della
    Enables user to switch between frames

--- main.py
+++ main.py
@@ -1,30 +1,63 @@
 from tkinter import *
 
-def  main():
-    root = Tk()
 
-    # size of the window
-    root.geometry("750x500")
+class LifePlusApp(Tk):
 
-    app = Window(root)
-    root.mainloop()
+    def __init__(self):
+        Tk.__init__(self)
+        self._frame = None
+        self.switch_frame(StartPage)
+
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
 
 
-class Window(Frame):
-
-    def __init__(self, master=None):
+class StartPage(Frame):
+    def __init__(self, master):
         Frame.__init__(self, master)
+        Label(self, text = "lifeplus", font = ("Helevetica", 30)).pack(side="top", fill="x", pady=5)
+        Button(self, text="What is your name?", command = lambda: master.switch_frame(GoalEntry)).pack()
+
+
+class GoalEntry(Frame):
+    def __init__(self, master):
+        self.goal = None
         self.master = master
-        self.init_window()
 
-    #Creation of init_window
-    def init_window(self):
+        Frame.__init__(self, master)
+        Label(self, text = "What is your goal?", font = ("Calibri", 20)).pack(side="top", fill="x", pady=5)
+        Entry(self, width = 50).pack()
+        Label(text=" ").pack()
+        Button(self, text = "Plan your goal!", command = lambda: master.switch_frame(HabitEntry)).pack()
 
-        # changing the title of our master widget
-        self.master.title("LifePlus")
+    def store_goal(self):
+        goal = StringVar()
+        goal.get()
+        self.goal = goal
+        # self.master(switch_frame(HabitEntry))
 
-        # allowing the widget to take the full space of the root window
-        self.pack(fill=BOTH, expand=1)
-        
 
-main()
+
+class HabitEntry(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        Label(self, text="How would you like to achieve it?", font=("Calibri", 20)).pack(side="top", fill="x", pady=5)
+        #
+        # habit = StringVar()
+        # global habit
+
+        Entry(self, width=50).pack()
+        Label(text=" ").pack()
+        Button(self, text = "Add habit", command = lambda: master.switch_frame(StartPage)).pack()   # Bind a function to command
+
+
+
+if __name__ == "__main__":
+    app = LifePlusApp()
+    app.mainloop()
+
+
